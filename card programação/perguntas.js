402cd911d71091bedc2b9004b57f5c308cb3a4ba
Maria Eduarda <maria.correa.lima24@escola.pr.gov.br>
2025-06-13 11:57:21 -0300

    Add files via upload

---
+++ card programação/perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'flor',
+    'Agapanto',
+    ' É uma planta muito resistente, mas prefere solos ricos e nutritivos, as cores são  branca e o azul/lilás.'
+)
+ 
+criaCartao(
+    'flor',
+    ' Brinco-de-Rainha',
+    'é nativo da América do Sul e é uma planta muito popular pelo mundo todo. Apresenta-se na maioria das vezes em ramagem pendente, as cores mais comuns são a cor de rosa, o azul, o violeta e o branco.'
+)
+
+criaCartao(
+    'flor',
+    'Clívia',
+    ' Tem origem africana, quase sempre encontradas em tons vermelhos e cor-de-laranja e muito difundidas para fins decorativos.'
+)
+criaCartao(
+    'arbusto',
+    'Coroa-de-cristo',
+    'Tem origem Africana (Madagáscar) e é comumente encontrada em tons vermelhos, amarelos e cor-de-laranja. Apesar de ter uma flor pequenina de aspeto delicado, este arbusto é tão espinhoso que é frequentemente utilizado como cerca viva.'
+)
+
+criaCartao(
+    'flor',
+    'Cravina',
+    'Semelhantes a mini-cravos, a cravina é uma flor muito difundida em Portugal. É nativa da Ásia e é frequente encontramos esta flor em diferentes cores, principalmente cores matizadas de branco, cor-de-rosa, roxo, lilás e vermelho.'
+)
+criaCartao(
+    'flor',
+    'Cíclame',
+    'Esta flor de origem Europeia pode ser encontrada nas cores vermelho, branco, cor-de-rosa e matizado. Tem uma aparência muito peculiar porque o formato das suas pétalas é invertido. É precisamente essa particularidade que faz desta planta algo único que apaixona qualquer um.'
+)
+criaCartao(
+    'flor',
+    'Crista de Galo',
+    'Crista de Galo é uma das espécies de celósia mais comuns em Portugal. É frequente ver esta flor de beleza incomparável e textura suave nos jardins públicos. Tem origem asiática e é comum encontrar esta flor nas cores vermelhas, branco, cor-de-rosa e amarelo.'
+)
+criaCartao(
+    'suculenta',
+    'Rosa de Pedra',
+    'Da família das suculentas, esta planta com origem mexicana é talvez a suculenta mais popular.Adquiriu o nome de rosa de pedra, dada a semelhança do formato. Quando dá flor, a rosa de pedra gera uma pequena flor em tons rosa.'
+)
+                     
+criaCartao(
+    'flor',
+    ' Verbena',
+    'Tem origem sul Americana, a flor da Verbena é constituída por mini-bouquets de pequenas flores. Pode ser encontrada em várias cores, mas as mais comuns são o vermelho, o roxo, o branco e o cor-de-rosa.'
+)
+
+criaCartao(
+    'flor',
+    'Íris',
+    'Apesar de serem frequentemente confundidas com os lírios, tratam-se de flores diferentes. A íris tem origem europeia e é encontrada naturalmente nas cores azúis ou brancas. Se a sua íris tiver uma cor que não seja azul ou branca, com certeza será um hibrido. Uma curiosidade acerca desta flor é que ela tem dentro das suas sépalas (pétalas secundárias) uma pequena penugem chamada de Barba de Íris.'
+)                       
+criaCartao(
+    'flor',
+    'Jacinto',
+    'O Jacinto tem origem europeia, asiática e africana e não tolera bem o calor. Por isso, no meio natural nasce no meio das árvores, para que não apanhe sol direto. Tem uma beleza muito particular, caracterizada por um conjunto de flores cilíndricas e apesar de existir em diversas cores, as mais comuns e apreciadas são a azul, a lilás, roxa, branco, vermelho, laranja e amarelo.'
+)                                  
